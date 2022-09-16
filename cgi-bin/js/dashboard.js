a59415ad7ce04708e0eb41b561931f18a6a0c0e3
Yimning <1148967988@qq.com>
2022-09-16 16:48:24 +0800

    修复已知BUG与优化代码

--- cgi-bin/js/dashboard.js
+++ cgi-bin/js/dashboard.js
@@ -12,7 +12,15 @@ $(document).ready(function(){
 })
 
 
-function imgFunc() {
-    document.click_submit.action = "/demo_test_fastcgi/fcgitest.fcgi?CMD=MENU&SELECT=0";
-    document.click_submit.submit();
+//清空cookie
+function clearCookie() {
+    var keys = document.cookie.match(/[^ =;]+(?=\=)/g);
+    if (keys) {
+        for (var i = keys.length; i--;) {
+            document.cookie = keys[i] + '=0;path=/;expires=' + new Date(0).toUTCString();//清除当前域名下的,例如：m.kevis.com
+            document.cookie = keys[i] + '=0;path=/;domain=' + document.domain + ';expires=' + new Date(0).toUTCString();//清除当前域名下的，例如 .m.kevis.com
+            document.cookie = keys[i] + '=0;path=/;domain=kevis.com;expires=' + new Date(0).toUTCString();//清除一级域名下的或指定的，例如 .kevis.com
+        }
+    }
+    console.log('已清除');
 }
